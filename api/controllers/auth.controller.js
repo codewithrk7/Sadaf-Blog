42b8b3b0ded22b08f50de01aaa02d405451d8cc6
codewithrk7 <rk5696608@gmail.com>
2024-01-19 02:08:49 +0530

    add OAuth functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -27,15 +27,15 @@ export const signup = async (req, res, next) => {
 
 };
 
-export const signin = async(req,res,next) => {
-    const {email , password} = req.body;
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
 
     if (!email || !password || email === '' || password === '') {
-        next(errorHandler(400,'All fields are required'));
+        next(errorHandler(400, 'All fields are required'));
     }
 
     try {
-        const validUser = await User.findOne({email});
+        const validUser = await User.findOne({ email });
         if (!validUser) {
             return next(errorHandler(404, 'User not found'));
         }
@@ -47,11 +47,42 @@ export const signin = async(req,res,next) => {
             {
                 id: validUser._id
             }, process.env.JWT_SECRET);
-            const {password: pass, ...rest} = validUser._doc;
-            res.status(200).cookie('access_token', token, {
-                httpOnly:true,
-            }).json(rest);
+        const { password: pass, ...rest } = validUser._doc;
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+        }).json(rest);
     } catch (error) {
         next(error);
     }
-}
+};
+
+export const google = async (req, res, next) => {
+    const { email, name, googlePhotoUrl } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const { password, ...rest } = user._doc;
+            res.status(200).cookie('access_token', token, {
+                httpOnly: true,
+            }).json(rest);
+        } else {
+            const generatePassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+            const hashedPassword = bcryptjs.hashSync(generatePassword, 10);
+            const newUser = new User({
+                username: name.toLowerCase().split(' ').join('') + Math.random().toString(9).slice(-4),
+                email,
+                password: hashedPassword,
+                profilePicture: googlePhotoUrl,
+            });
+            await newUser.save();
+            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const { password, ...rest } = newUser._doc;
+            res.status(200).cookie('access_token', token, {
+                httpOnly: true,
+            }).json(rest);
+        }
+    } catch (error) {
+
+    }
+};
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    profilePicture: {
+        type: String,
+        default: 'https://tse3.mm.bing.net/th?id=OIP.d_V5Ti60n3mJuPheks-k4AHaHa&pid=Api&P=0&h=220',
+    },
 }, { timestamps: true }
 );
 
